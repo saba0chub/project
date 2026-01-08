230462ed7ac5320f75db4bb64479e9618ecdba90
saba0chub <saba2009chub@gmail.com>
2026-01-08 18:27:50 +0400

    davwere kodi romelic irchevs sityvas, mis pirvel asos da danarchen asoebs cvlis "_" ebad. while true gamoiyenet chamoxrchobis satamashot

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+from random import randint         # gamovidzaxe randint
+
+words = [
+    "house", "car", "tree", "book", "phone",
+    "table", "street", "sun", "sea", "wind",         #random sityvebi romelic chemi mogoniuli araa
+    "friend", "city", "time", "window", "door",
+    "chair", "computer", "mountain", "sky", "flower"
+]
+
+
+
+wordInd = randint(0, 20)    # am cvlads mieniweba random cifri 0-20 chatvlit romelic gamoieneba index ad
+
+secretWord = words[wordInd]     # am cvlads wordInd is daxmarebit eniweba random sityva
+
+
+
+letter1 = secretWord[0] # am cvlads eniweba random sityvis pirveli aso (chamoxrchobanashi pirveli aso xo cnobilia)
+
+
+for i in range(len(secretWord)- 1):
+    letter1 += " _"                     # pirveli asos garda yvela asos magivrad iwereba _
+
+
+
+
+while True:
+    print(f"sityvaa ------->   {letter1}")
+    a = input("gamoicani aso ----> ")   # sheuchereblad gekitxeba asos sheyvanas
+
